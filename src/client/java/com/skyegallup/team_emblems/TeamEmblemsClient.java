2c87ed3afb900da1eb28e2aa19d0d564e6597d4e
Skylar Gallup <cwg7336@rit.edu>
2024-12-30 19:22:52 -0600

    Initialized 1.20.1 project

---
+++ src/client/java/com/skyegallup/team_emblems/TeamEmblemsClient.java
@@ -0,0 +1,10 @@
+package com.skyegallup.team_emblems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeamEmblemsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
